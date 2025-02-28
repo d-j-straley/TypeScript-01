28432e9680375e55a45712f4b0fa6cc385e88d5b
David J. Straley <djstraley@sandyhook.com>
2025-02-28 14:02:35 -0600

    demo of readonly array

---
+++ array-02.js
@@ -0,0 +1,4 @@
+var names2 = ["Dylan"];
+names2.push("Jack"); // Error: Property 'push' does not exist on type 'readonly string[]'.
+// try removing the readonly modifier and see if it works?
+console.log(names2);
---
+++ array-02.ts
@@ -0,0 +1,6 @@
+const names2:  readonly string[] = ["Dylan"];
+
+names2.push("Jack"); // Error: Property 'push' does not exist on type 'readonly string[]'.
+
+// try removing the readonly modifier and see if it works?
+console.log(names2);
