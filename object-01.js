238ba92d958393cd590d141721cc33764e32bb43
David J. Straley <djstraley@sandyhook.com>
2025-03-01 21:44:11 -0600

    added types

--- object-01.js
+++ object-01.js
@@ -1,8 +1,10 @@
-var car = {
-    type: "Sedan",
-    model: "Toyota",
+const car = { type: String, model: String, year: number } =
+{
+    type: "Toyota",
+    model: "Sedan",
     year: 2020
 };
+
 console.log(car.type);
 console.log(car.model);
 console.log(car.year);
