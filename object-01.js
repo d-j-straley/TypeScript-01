e08d3a7004fd9948adc8c0dddb3c2f14a6b7ae6d
David J. Straley <djstraley@sandyhook.com>
2025-03-01 21:27:24 -0600

    example of a TypeScript object type

---
+++ object-01.js
@@ -0,0 +1,8 @@
+var car = {
+    type: "Sedan",
+    model: "Toyota",
+    year: 2020
+};
+console.log(car.type);
+console.log(car.model);
+console.log(car.year);
---
+++ object-01.ts
@@ -0,0 +1,10 @@
+const car: { type: string, model: string, year: number } = {
+    type: "Sedan",
+    model: "Toyota",
+    year: 2020
+};
+
+console.log(car.type);
+console.log(car.model); 
+console.log(car.year);
+ 
