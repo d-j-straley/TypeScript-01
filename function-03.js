46dd9c9f8cc928ad31d7f85b5a348bb2d42307f7
David J. Straley <djstraley@sandyhook.com>
2025-03-12 14:58:51 -0500

    demo of declaring the input parameter types of a function

---
+++ function-03.js
@@ -0,0 +1,5 @@
+function multiply(a, b) {
+    return a * b;
+}
+console.log(multiply(2, "5")); // 10
+console.log(multiply(3, 6)); // 18
---
+++ function-03.ts
@@ -0,0 +1,6 @@
+function multiply(a: number, b: number) {
+  return a * b;
+}
+
+console.log(multiply(2, 5)); // 10
+console.log(multiply(3, 6)); // 18
