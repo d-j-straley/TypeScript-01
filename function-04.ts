777c1b8c8df1d272e01963124e6d1ea67be15e60
David J. Straley <djstraley@sandyhook.com>
2025-03-12 15:06:58 -0500

    explanation of additional code needed to make sense out of default parameter values

--- function-04.ts
+++ function-04.ts
@@ -1,4 +1,10 @@
 // the `?` operator here marks parameter `c` as optional
+//note the following syntax: (c || 0) in the function body
+//this is a way to provide a default value for the parameter `c` if it is not provided by the caller
+//if `c` is not provided, then it will be `undefined` and `undefined` is falsy in JavaScript
+//so, `c || 0` will return `0` if `c` is not provided
+//this is a common way to provide default values in JavaScript
+
 function add(a: number, b: number, c?: number) {
   return a + b + (c || 0);
 }
